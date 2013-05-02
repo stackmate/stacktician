da73b5b171961a4a27dfa35f5585122f61c17145
Chiradeep Vittal <chiradeep@apache.org>
2013-05-02 14:59:22 -0700

    Add static pages
    Add about page with some static pages tests
    
    refactor static pages and tests
    
    Add spork to speed up rspec

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '3.2.13'
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
+  gem 'spork', '0.9.2'
 end
 
 # Gems used only for assets and not required
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  let(:base_title) { "Stack*tician" }
+
+  describe "Home page" do
+
+    it "should have the h1 'Stack*tician'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Stack*tician')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "#{base_title} | Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "#{base_title} | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                    :text => "#{base_title} | About")
+    end
+  end
+end
