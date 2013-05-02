66eefb787757cdb1bf09daeecdcebc7390d3e61c
Chiradeep Vittal <chiradeep@apache.org>
2013-05-02 14:51:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stacktician::Application.config.secret_token = '25f3b2976777ab7098e28b9c50dcf7366a0c31dee185cbf1df9de63c8970201490461906e727d89690808412d1b026f6db2c9942303f82a064430ecd546a890a'
