aa14bf9ea21f5e51735735ecfadaae07e8517be6
Chiradeep Vittal <chiradeep@apache.org>
2013-05-03 13:16:27 -0700

    user model

--- Gemfile
+++ Gemfile
@@ -2,11 +2,16 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
 gem 'bootstrap-sass', '2.1'
+gem 'bcrypt-ruby', '3.0.1'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
   gem 'spork', '0.9.2'
+end
+
+group :development do
+  gem 'annotate', '2.5.0'
 end
 
 # Gems used only for assets and not required
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Stacktician::Application.routes.draw do
-    root to: 'static_pages#home'
+  get "users/new"
 
-    match '/help',    to: 'static_pages#help'
-    match '/about',   to: 'static_pages#about'
+  root to: 'static_pages#home'
+
+  match '/signup',  to: 'users#new'
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
 
 end
