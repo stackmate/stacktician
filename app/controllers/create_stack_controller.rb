9904bc2bc90c0656e028f946874d6532ff5b9476
Chiradeep Vittal <chiradeep@apache.org>
2013-05-08 22:59:35 -0700

    wizard works

--- app/controllers/create_stack_controller.rb
+++ app/controllers/create_stack_controller.rb
@@ -3,14 +3,40 @@ class CreateStackController < ApplicationController
   steps :select_template, :enter_params, :launch
 
   def show
-      @stack = current_user.stacks.build()
-      session[:current_stack] = @stack
-      render_wizard @stack
+      case step
+         when :select_template
+           @stack = current_user.stacks.build()
+           session[:current_stack] = @stack
+           logger.debug "stack: show :select_template #{@stack.attributes.inspect}"
+        when :enter_params
+          @stack = session[:current_stack]
+          logger.debug "stack: show :enter_params #{@stack.attributes.inspect}"
+          stack_template = StackTemplate.find_by_id(@stack.stack_template_id)
+          #logger.debug "stack_template: #{@stack_template.attributes.inspect}"
+          @descr = stack_template.description
+        when :launch
+          @stack = session[:current_stack]
+          logger.debug "stack: show :launch #{@stack.attributes.inspect}"
+      end
+      render_wizard 
   end
 
   def update
-      @stack = session[:current_stack]
-      @stack.update_attributes(params[:stack])
+      case step
+        when :enter_params
+          @stack = session[:current_stack]
+          logger.debug "stack: update :enter_params #{@stack.attributes.inspect}"
+          @stack.update_attributes(params[:stack])
+        when :select_template
+          @stack = session[:current_stack]
+          @stack.update_attributes(params[:stack])
+          logger.debug "stack: update :select_template #{@stack.attributes.inspect}"
+        when :launch
+          @stack = session[:current_stack]
+          @stack.update_attributes(params[:stack])
+          logger.debug "stack: update :launch #{@stack.attributes.inspect}"
+      end
+
       render_wizard @stack
   end
 
--- app/helpers/create_stack_helper.rb
+++ app/helpers/create_stack_helper.rb
@@ -1,2 +1,5 @@
 module CreateStackHelper
+    def current_stack_template (stack)
+        StackTemplate.find_by_template_name(stack.stack_template)
+    end
 end
