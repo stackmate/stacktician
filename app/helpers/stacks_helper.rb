1e3d16a2aa7bc73b7deaaa82a632fbb8c8ec9553
Chiradeep Vittal <chiradeep@apache.org>
2013-05-29 16:34:27 -0700

    Add styling to resource status (warning/error/info/success)

--- app/helpers/stacks_helper.rb
+++ app/helpers/stacks_helper.rb
@@ -13,4 +13,27 @@ module StacksHelper
       cls
   end
 
+  def resource_status_style(resource)
+      # CREATE_IN_PROGRESS | CREATE_FAILED | CREATE_COMPLETE | DELETE_IN_PROGRESS | DELETE_FAILED | DELETE_COMPLETE 
+      cls = 'info'
+      case resource.status
+      when /_IN_PROGRESS/
+        cls = 'warning'
+      when /_FAILED/
+        cls = 'error'
+      when /_COMPLETE/
+        cls = 'success'
+      end
+      cls
+  end
+
+  def uri?(physical_id)
+    uri = URI.parse(physical_id)
+    %w( http https ).include?(uri.scheme)
+    rescue URI::BadURIError
+      false
+    rescue URI::InvalidURIError
+      false
+  end
+
 end
